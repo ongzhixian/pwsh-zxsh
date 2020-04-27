dddd4712b3cbaa8dc337962bc04f1e75a8120540
zhixian <zhixian@hotmail.com>
2020-04-27 23:52:13 +0800

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pwsh-zxsh
-My personalised PowerShell Core "shell" module
+# Overview
+
+My personalised "shell" for PowerShell Core.
+
+Using PowerShell Core 7.0
+
+## Module name
+
+WebHost
+
+## Usage
+
+`Start-WebHost`
+
+Defaults to localhost (127.0.0.1), port 2194
+
+`Start-WebHost -Port 4848`
+
+Run and host on (127.0.0.1), port 4848
+
---
+++ zxsh/zxsh.psm1
@@ -0,0 +1,35 @@
+<#
+ .Synopsis
+  Set title of console Window
+
+ .Description
+  Set title of console Window
+
+ .Parameter $args
+  Set title of console Window to $args
+
+ .Example
+   # Set title of console Window to 'ps7'
+   Title ps7
+#>
+
+$Script:PrevPwd = $null
+$Script:BranchName = $null
+
+Function Set-Title { (Get-Host).UI.RawUI.WindowTitle = $args }
+
+Function Prompt {
+    $current_path = (Get-Location).Path
+    if ($Script:PrevPwd -ne $current_path)
+    {
+        $Script:BranchName = $(git branch --show-current)
+        $Script:PrevPwd = $current_path
+    }    
+    "`e[32m$env:USERDOMAIN>$env:USERNAME`e[39m $PWD $(if ($LASTEXITCODE -eq 0) { "`e[36m($Script:BranchName)`e[39m" })`nPS> "
+}
+
+New-Alias -Name title -Value Set-Title
+
+# Module member export definitions
+Export-ModuleMember -Function Set-Title, Prompt
+Export-ModuleMember -Alias title
