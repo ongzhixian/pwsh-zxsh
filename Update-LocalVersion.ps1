5fcb9055593db1bdafe04e3426006f60fa818d43
zhixian <zhixian@hotmail.com>
2020-05-05 22:56:07 +0800

    Fix LocalVersion update script to match directory changes

--- Update-LocalVersion.ps1
+++ Update-LocalVersion.ps1
@@ -5,6 +5,6 @@
 # Set-PSRepository -Name 'pwsh-repository' -InstallationPolicy Trusted
 
 Get-ChildItem 'C:\Apps\PwshRepository' | Where-Object { $_.Name -like 'zxsh*' } | ForEach-Object { Remove-Item $_ }
-Publish-Module -Path 'C:\src\github.com\ongzhixian\pwsh-zxsh\pwsh-zxsh\zxsh' -Repository 'pwsh-repository' -Force
+Publish-Module -Path 'C:\src\github.com\ongzhixian\pwsh-zxsh\code\zxsh' -Repository 'pwsh-repository' -Force
 Install-Module 'zxsh' -Repository 'pwsh-repository' -Force
 Import-Module zxsh -Force
