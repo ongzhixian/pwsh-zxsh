0216217f442d44ecc14ef45714b3c70fe7f54a1e
zhixian <zhixian@hotmail.com>
2020-06-09 00:02:31 +0800

    add another example of capturing exception using pester (the more correct way?)

--- test/ako.test.ps1
+++ test/ako.test.ps1
@@ -1,5 +1,5 @@
 Describe "/api/echo" {
-    
+
     It "?msg=asd" {
         $a = Invoke-RestMethod -Uri "http://localhost:8080/api/echo?msg=asd"
         $a | Should Be "re:asd"
@@ -18,5 +18,11 @@ Describe "/api/echo" {
             $ex = $_.Exception
             $ex.Response.StatusCode -eq 400 | Should Be $true
         }
+    }
+
+    It "no-querystring2" {
+        {
+            $a = Invoke-RestMethod -Uri "http://localhost:8080/api/echo" 
+        } | Should Throw "Response status code does not indicate success: 400 (BAD REQUEST)."
     }
 }
