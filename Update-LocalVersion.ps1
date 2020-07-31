84ce31f6c057f5ecfb8b955ca4f9b30202dfa97b
Ong Zhixian <zhixian@hotmail.com>
2020-07-31 08:58:47 +0800

    Add support for Acadian

--- Update-LocalVersion.ps1
+++ Update-LocalVersion.ps1
@@ -1,10 +1,37 @@
-#
-#
+# Script use to update local PowerShell repository (for development)
 # 
+# Notes:
 # Register-PSRepository -Name 'pwsh-repository' -SourceLocation 'C:\Apps\PwshRepository'
 # Set-PSRepository -Name 'pwsh-repository' -InstallationPolicy Trusted
+#
+switch ($env:COMPUTERNAME)
+{
+    "ZOGA330" {
+        Get-ChildItem 'C:\Apps\PwshRepository' | Where-Object { $_.Name -like 'zxsh*' } | ForEach-Object { Remove-Item $_ }
+        Publish-Module -Path 'C:\src\github.com\ongzhixian\pwsh-zxsh\code\zxsh' -Repository 'pwsh-repository' -Force
+        Install-Module 'zxsh' -Repository 'pwsh-repository' -Force -AllowClobber
+        Import-Module zxsh -Force
+        break
+    }
 
-Get-ChildItem 'C:\Apps\PwshRepository' | Where-Object { $_.Name -like 'zxsh*' } | ForEach-Object { Remove-Item $_ }
-Publish-Module -Path 'C:\src\github.com\ongzhixian\pwsh-zxsh\code\zxsh' -Repository 'pwsh-repository' -Force
-Install-Module 'zxsh' -Repository 'pwsh-repository' -Force
-Import-Module zxsh -Force
+    "SG00-L4579" {
+        Get-ChildItem 'C:\Apps\PwshRepository' | Where-Object { $_.Name -like 'zxsh*' } | ForEach-Object { Remove-Item $_ }
+        Publish-Module -Path 'C:\src\pwsh-zxsh\code\zxsh' -Repository 'pwsh-repository' -Force
+        Install-Module 'zxsh' -Repository 'pwsh-repository' -Force -AllowClobber
+        Import-Module zxsh -Force
+        break
+    }
+
+    "ACADIAN" {
+        Get-ChildItem 'D:\Apps\PwshRepository' | Where-Object { $_.Name -like 'zxsh*' } | ForEach-Object { Remove-Item $_ }
+        Publish-Module -Path 'D:\src\github.com\ongzhixian\pwsh-zxsh\code' -Repository 'pwsh-repository' -Force
+        Install-Module 'zxsh' -Repository 'pwsh-repository' -Force -AllowClobber
+        Import-Module zxsh -Force
+        break
+    }
+
+    default {
+        Write-Error "No setup found for $($env:COMPUTERNAME)"
+    }
+
+}
